2db8bdc53ec26a8748afcd3e943361c9b4c6d59e
Mohammed-Shoaib <shoaib98libra@gmail.com>
2020-03-09 21:58:43 +0400

    Add support to load and serialize the dataset

---
+++ scripts/serialize_data.py
@@ -0,0 +1,119 @@
+import os
+import sys
+import keras
+import argparse
+from utils import *
+from typing import Tuple
+
+
+
+def load_data(path: str) -> Tuple[np.ndarray, np.ndarray]:
+	"""
+	Takes a path to the dataset.
+	Each folder must be a label containing the raw data.
+	
+	Arguments:
+		path {str} -- path to the dataset 
+	
+	Returns:
+		Tuple[np.ndarray, np.ndarray] -- tuple of two items (images or xs, labels or ys)
+	"""
+	xs, ys = [], []
+	labels = os.listdir(path)
+
+	# open each label folder
+	for label in labels:
+		print(f'Loading label {label}...')
+		files = os.listdir(os.path.join(path, label))
+		
+		# open each image and store in data
+		for file_name in files:
+			file_path = os.path.join(path, label, file_name)
+			xs.append(read_image(file_path))
+			ys.append(mapping[label])
+
+	return np.asarray(xs), np.asarray(ys)
+
+
+
+def preprocess_data(xs: np.ndarray, ys: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
+	"""
+	1. Resizes each image to a shape [SHAPE✕SHAPE✕CHANNELS] with padding to keep the aspect ratio.
+	2. Shuffles the xs & ys together.
+	3. One-hot encodes the labels or ys.
+	
+	Arguments:
+		xs {np.ndarray} -- an array of images
+		ys {np.ndarray} -- the label for each image in xs
+	
+	Returns:
+		Tuple[np.ndarray, np.ndarray] -- tuple of two items (images or xs, labels or ys)
+	"""
+	print(f'Resizing the xs to a shape of [{SHAPE}✕{SHAPE}✕{CHANNELS}]...')
+	for i, img in enumerate(xs):
+		xs[i] = resize_image(img)
+	
+	print('Shuffling the data...')
+	shuffle_in_unison(xs, ys)
+	shuffle_in_unison(xs, ys)
+	shuffle_in_unison(xs, ys)
+	
+	print('One-hot encoding the ys...')
+	ys = keras.utils.to_categorical(ys, NUM_CLASSES).astype(int)
+	
+	return xs, ys
+
+
+
+def split_data(xs: np.ndarray, ys: np.ndarray, offset: float = 0.8) -> Tuple[Tuple[np.ndarray, np.ndarray], Tuple[np.ndarray, np.ndarray]]:
+	"""
+	Splits the dataset into train and test sets.
+	
+	Arguments:
+		xs {np.ndarray} -- an array of images
+		ys {np.ndarray} -- the label for each image in xs
+	
+	Keyword Arguments:
+		offset {float} -- train:test split ratio (default: {0.8})
+	
+	Returns:
+		Tuple[Tuple[np.ndarray, np.ndarray], Tuple[np.ndarray, np.ndarray]] -- tuple of two items (train set, test set)
+	"""
+	print('Splitting the data...')
+	offset = int(len(xs) * offset)
+	train_xs, train_ys = xs[:offset], ys[:offset]
+	test_xs, test_ys = xs[offset:], ys[offset:]
+
+	return (train_xs, train_ys), (test_xs, test_ys)
+
+
+
+# add keyword arguments
+parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument('--data', '-d', help='Path to an input dataset folder', required=True)
+parser.add_argument('--output', '-o', help='Path to a directory to output serialized objects', required=True)
+parser.add_argument('--mapping', '-m', help='Path to input json mapping of letters', default='mapping.json')
+args = parser.parse_args()
+
+
+
+if __name__ == '__main__':
+	# error handling
+	if not os.path.exists(args.data):
+		sys.exit('The path to the dataset folder does not exist.')
+	elif not os.path.exists(args.mapping):
+		sys.exit('The path to the mapping does not exist.')
+	elif not os.path.exists(args.output):
+		os.makedirs(args.output)
+	
+	# get the data
+	xs, ys = load_data(args.data)
+	xs, ys = preprocess_data(xs, ys)
+	(train_xs, train_ys), (test_xs, test_ys) = split_data(xs, ys)
+
+	# serialize objects
+	variables = ['xs', 'ys', 'train_xs', 'train_ys', 'test_xs', 'test_ys']
+	for v in variables:
+		print(f'Serializing the {v}...')
+		path = os.path.join(args.output, v)
+		serialize(locals()[v], path)
--- scripts/utils.py
+++ scripts/utils.py
@@ -1,7 +1,13 @@
 import cv2
 import json
+import pickle
 import numpy as np
 from config import *
+from typing import TypeVar
+
+
+
+T = TypeVar('T')
 
 
 
@@ -35,3 +41,76 @@ def read_image(path: str) -> np.ndarray:
 	if len(img.shape) != 3:
 		img = cv2.cvtColor(img, cv2.COLOR_GRAY2RGB)
 	return np.asarray(img)
+
+
+
+def serialize(data: T, path: str) -> None:
+	"""
+	Serializes the given data and stores the binary in the given path.
+	
+	Arguments:
+		data {T} -- object to serialize
+		path {str} -- path to output serialized object
+	"""
+	with open(path, 'wb') as f:
+		pickle.dump(data, f, protocol=pickle.HIGHEST_PROTOCOL)
+
+
+
+def deserialize(path: str) -> T:
+	"""
+	Deserializes the binary in the given path.
+	
+	Arguments:
+		path {str} -- path to serialized object
+	
+	Returns:
+		T -- deserialized object
+	"""
+	with open(path, 'rb') as f:
+		data = pickle.load(f)
+	return data
+
+
+
+def resize_image(img: np.ndarray) -> np.ndarray:
+	"""
+	Resizes an image in (width, height, channels) format with padding to keep the aspect ratio.
+	
+	Arguments:
+		img {np.ndarray} -- an array of image pixels
+	
+	Returns:
+		np.ndarray -- resized image of shape [SHAPE✕SHAPE✕CHANNELS]
+	"""
+	# resize the image
+	old_shape = img.shape[1::-1]
+	ratio = SHAPE / max(old_shape)
+	new_shape = tuple([int(dim * ratio) for dim in old_shape])
+	img = cv2.resize(img, new_shape)
+
+	# pad the image
+	color = [0, 0, 0]
+	w, h = [SHAPE - dim for dim in new_shape]
+	top, bottom = h // 2, h - h // 2
+	left, right = w // 2, w - w // 2
+	img = cv2.copyMakeBorder(img, top, bottom, left, right, cv2.BORDER_CONSTANT, value=color)
+
+	return img
+
+
+
+def shuffle_in_unison(a: np.ndarray, b: np.ndarray) -> None:
+	"""
+	Shuffles 2 numpy arrays together inplace.
+	AssertionError if the lengths of a & b are not the same.
+	
+	Arguments:
+		a {np.ndarray} -- a numpy array
+		b {np.ndarray} -- a numpy array
+	"""
+	assert len(a) == len(b)
+	rng_state = np.random.get_state()
+	np.random.shuffle(a)
+	np.random.set_state(rng_state)
+	np.random.shuffle(b)
